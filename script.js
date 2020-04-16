bc861b477bcb5e54ba8186d681e264ec15a1d83b
rehlaender <avilamario16@gmail.com>
2020-04-15 22:38:47 -0500

    add loading state

--- script.js
+++ script.js
@@ -2,6 +2,81 @@ var removeOpenClass = function (event) {
   document.getElementById('bigimagecontainer').classList.remove('open');
 }
 
+var CARTONES = [
+  'https://soyjehu.github.io/assets/cartones/acupuntura-derecho.jpg',
+  'https://soyjehu.github.io/assets/cartones/brazos-peludos.jpg',
+  'https://soyjehu.github.io/assets/cartones/desayuno.jpg',
+  'https://soyjehu.github.io/assets/cartones/fila.jpg',
+];
+
+var HISTORIETAS = [
+  'https://soyjehu.github.io/assets/historietas/Comic_Banarse.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Dolores.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Domingo_Apocaliptico.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Domingo_Celular.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Domingo_Gemelo.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Domingo_Mosqueado.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Martes.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Pajaro_Negro.jpg',
+  'https://soyjehu.github.io/assets/historietas/Comic_Transgenico.jpg',
+  'https://soyjehu.github.io/assets/historietas/El_hombre_cabeza_de_lavadora.jpg',
+];
+
+var ILUSTRACIONES = [
+  'https://soyjehu.github.io/assets/ilustraciones/autorretrato.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/carro.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/garza_asada.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/gato_estirandose.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/jiustonenvivo.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/monje.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/sabila.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/taquitos_con_marco.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/tuna_no.jpg',
+  'https://soyjehu.github.io/assets/ilustraciones/yo_cuarentena.jpg',
+];
+
+var LIBROS = [
+  'https://soyjehu.github.io/assets/libroscompletos/ComediantesDeprimidos.png',
+  'https://soyjehu.github.io/assets/libroscompletos/FuiArrastradoDebajoDeLaCamaPorUnDemonio-Jehu.png',
+  'https://soyjehu.github.io/assets/libroscompletos/La-luz-que-no-ilumina-todo-Jehu-1.png',
+  'https://soyjehu.github.io/assets/libroscompletos/La-senora-que-volaba.png',
+];
+
+var loadArray = function (datasetArray, option) {
+  datasetArray.forEach((element, key) => {
+    var id = option + (key + 1);
+    console.log(id, 'what');
+    var _img = document.getElementById(id);
+    var newImg = new Image;
+    newImg.onload = function () {
+      _img.style.backgroundImage = "url(" + this.src + ")";
+      _img.classList.remove("loading");
+      console.log('ready', this.src);
+    }
+    newImg.src = element;
+  });
+};
+
+var loadSection = function (option) {
+  switch (option) {
+    case 'cartones':
+      loadArray(CARTONES, option);
+      break;
+    case 'historietas':
+      loadArray(HISTORIETAS, option);
+      break;
+    case 'ilustraciones':
+      loadArray(ILUSTRACIONES, option);
+      break;
+    case 'libros':
+      loadArray(LIBROS, option);
+      break;
+    default:
+      break;
+  }
+}
+
+
 window.onload = function () {
   var BTN_CARTONES = document.getElementById('cartonesButton');
   var BTN_HISTORIETAS = document.getElementById('historietasButton');
@@ -14,6 +89,8 @@ window.onload = function () {
   var BACK_LIBROS = document.getElementById('librosBack');
 
   var changeRoute = function (event) {
+    console.log(event.target.dataset.name, 'open this');
+    loadSection(event.target.dataset.name);
     document.getElementById(event.target.dataset.name).classList.add("active");
   }
 
